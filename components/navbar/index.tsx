db89be2d043cdfa7b0f80a89498c312623c305f5
Ryan Paige <oshmagoshzz@gmail.com>
2022-01-11 18:10:50 -0500

    nav buttons added

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,18 +1,35 @@
-import { Button, IconButton, useColorMode } from "@chakra-ui/react";
+import { useRouter } from 'next/router'
+import Link from "next/link";
+import { Button, IconButton, ButtonGroup, useColorMode } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import NavBarContainer from "./Container";
-import Link from "next/link";
 
 type NavBarProps = React.PropsWithChildren<{}>;
 
 const Navbar = ( props: NavBarProps ) => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const router = useRouter()
 
   return (
     <NavBarContainer { ...props }>
-      <Link href="/poetry" passHref>
-      <Button> Poetry </Button>
-      </Link>
+      <ButtonGroup mx={5}>
+        <Link href="/" passHref>
+          <Button 
+            aria-label='Go To Book Page'
+            bg={router.pathname === '/poetry' ? "red.900" : undefined}
+            _focus={{outline: "hidden"}}
+          > 
+            Books </Button>
+        </Link>
+        <Link href="/poetry" passHref>
+          <Button 
+            aria-label='Go To Poetry Page'
+            bg={router.pathname === '/poetry' ? undefined: "red.900" }
+            _focus={{outline: "hidden"}}
+          > 
+            Poetry </Button>
+        </Link>
+      </ButtonGroup>
       <IconButton 
         aria-label={ "Color Toggle Button" }
         onClick={ toggleColorMode } 
