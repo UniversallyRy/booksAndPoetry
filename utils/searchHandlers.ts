f026b1ce534e0fd847caf9bd960c93824af5ef28
Ryan Paige <oshmagoshzz@gmail.com>
2022-01-22 16:04:26 -0500

    code spacing

--- utils/searchHandlers.ts
+++ utils/searchHandlers.ts
@@ -3,46 +3,46 @@ import { Axios } from "axios";
 import { BookProps } from "../components/bookCover/CoverCard";
 
 export type BookObjProps = {
-    searchInput: string;
-    searchBookList: never[];
-    book: BookProps;
+  searchInput: string;
+  searchBookList: never[];
+  book: BookProps;
 }
 
 export type SearchProps = {
-    e: {target: { value: string | undefined}};
-    axios: Axios;
-    bookObj: BookObjProps; 
-    setBook: Dispatch<SetStateAction<BookObjProps>>;
+  e: {target: { value: string | undefined}};
+  axios: Axios;
+  bookObj: BookObjProps; 
+  setBook: Dispatch<SetStateAction<BookObjProps>>;
 }
 
 export type onClickProps = {
-    id: string;
-    bookObj: BookObjProps;
-    setBook: Dispatch<SetStateAction<BookObjProps>>;
+  id: string;
+  bookObj: BookObjProps;
+  setBook: Dispatch<SetStateAction<BookObjProps>>;
 }
 
 export const getSearchedBooks = ({ e, axios, bookObj, setBook }: SearchProps ) => {
-    const searchedInput = e.target.value;
+  const searchedInput = e.target.value;
   
-    if (searchedInput) {
-      axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchedInput}`)
-        .then(( res ) => res.data )
-        .then((data ) => {
-          if ( data.totalItems > 0 ) {
-            setBook({ 
-              ...bookObj,
-              searchInput: searchedInput,
-              book: data.items
-            });
-          }
-        })
-        .catch(( err ) => console.log( err ));
-    } else {
-      setBook({ 
-        ...bookObj,
-        searchBookList: [], 
-      });
-    }
+  if (searchedInput) {
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchedInput}`)
+      .then(( res ) => res.data )
+      .then((data ) => {
+        if ( data.totalItems > 0 ) {
+          setBook({ 
+            ...bookObj,
+            searchInput: searchedInput,
+            book: data.items
+          });
+        }
+      })
+      .catch(( err ) => console.log( err ));
+  }else {
+    setBook({ 
+      ...bookObj,
+      searchBookList: [], 
+    });
+  }
 }
 
 export const getSearchedPoems = ({ e, axios, poemObj, setPoem }: any ) => {
@@ -76,25 +76,25 @@ export const getSearchedPoems = ({ e, axios, poemObj, setPoem }: any ) => {
 };
   
 export const bookHandler = ({ id, bookObj, setBook }: onClickProps ) => {
-    const { searchBookList } = bookObj;
-    const targetItem = searchBookList.filter(( item:{ id: string }) => { return item.id === id })[0];
+  const { searchBookList } = bookObj;
+  const targetItem = searchBookList.filter(( item:{ id: string }) => { return item.id === id })[0];
     
-    setBook({
-      searchInput: "",
-      searchBookList: [],
-      book: targetItem
-    });
+  setBook({
+    searchInput: "",
+    searchBookList: [],
+    book: targetItem
+  });
 };
 
 export const poemHandler = async ({ id, axios, poemObj, setPoem }: any ) => {
-    const { searchPoemList, searchedInput } = poemObj;
-    const targetItem = searchPoemList.filter(( item:{ id: string }) => { return item.id === id })[0];
-    const res = await axios.get(`https://poetrydb.org/title,author/${targetItem.title};${searchedInput}`);
-    const poem = await res.data[0];
-    setPoem({
-      searchedInput: "",
-      searchPoemList: [],
-      poem: poem
-    });
+  const { searchPoemList, searchedInput } = poemObj;
+  const targetItem = searchPoemList.filter(( item:{ id: string }) => { return item.id === id })[0];
+  const res = await axios.get(`https://poetrydb.org/title,author/${targetItem.title};${searchedInput}`);
+  const poem = await res.data[0];
+  setPoem({
+    searchedInput: "",
+    searchPoemList: [],
+    poem: poem
+  });
 };
   
